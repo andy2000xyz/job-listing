50b31d6c714670a4ed776cd874f11047a1ef6636
andy2000xyz <andy2000xyz@hotmail.com>
2017-01-05 16:25:02 +0800

    part2-step5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  def admin?
+      email == '495145788@qq.com'
+    end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   resources :jobs
   root 'jobs#index'
+
+end
+
+namespace :admin do
+  resources :jobs
 end
