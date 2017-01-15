cd69613f95af8b235c0213f3ce149e971a6b299a
andy2000xyz <andy2000xyz@hotmail.com>
2017-01-15 21:53:12 +0800

    front page

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,17 @@ Rails.application.routes.draw do
         post :hide
       end
       resources :resumes
-      
+
     end
   end
 
+  resources :welcome do
+
+  end
 
     resources :jobs do
       resources :resumes
     end
 
-    root 'jobs#index'
+    root 'welcome#index'
 end
